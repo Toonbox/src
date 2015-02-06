887bd7250ca5258091cc613f6035a583523a0906
Puggyblue999 <Puggyblue999@users.noreply.github.com>
2015-02-06 17:39:37 -0500

    Fixed indenting

--- toontown/toontowngui/ToontownLoadingScreen.py
+++ toontown/toontowngui/ToontownLoadingScreen.py
@@ -43,27 +43,27 @@ class ToontownLoadingScreen:
 	}
 	
 	def __init__(self):
-		self.__expectedCount = 0
-		self.__count = 0
-		self.gui = loader.loadModel('phase_3/models/gui/progress-background.bam')
-		self.title = DirectLabel(guiId='ToontownLoadingScreenTitle', parent=self.gui, relief=None, pos=(base.a2dTopCenter+0, 0, 0.235), text='', textMayChange=1, text_scale=0.1, text_fg=(0.00, 0.58, 0.996, 1), text_align=TextNode.ACenter, text_font=ToontownGlobals.getSignFont())
-		self.waitBar = DirectWaitBar(guiId='ToontownLoadingScreenWaitBar', parent=self.gui, frameSize=(-0.75, 0.75, -0.02, 0.02), pos=(0, 0, -0.85), text='')
-		return
+	    self.__expectedCount = 0
+	    self.__count = 0
+	    self.gui = loader.loadModel('phase_3/models/gui/progress-background.bam')
+	    self.title = DirectLabel(guiId='ToontownLoadingScreenTitle', parent=self.gui, relief=None, pos=(base.a2dTopCenter+0, 0, 0.235), text='', textMayChange=1, text_scale=0.1, text_fg=(0.00, 0.58, 0.996, 1), text_align=TextNode.ACenter, text_font=ToontownGlobals.getSignFont())
+	    self.waitBar = DirectWaitBar(guiId='ToontownLoadingScreenWaitBar', parent=self.gui, frameSize=(-0.75, 0.75, -0.02, 0.02), pos=(0, 0, -0.85), text='')
+	    return
 		
 	def destroy(self):
-		pass
+	    pass
 		
 	def getTip(self, todo):
-		pass
+	    pass
 		
 	def begin(self, todo, todo1, todo2, todo3):
-		pass
+	    pass
 		
 	def end(self):
-		pass
+	    pass
 		
 	def abort(self):
-		pass
+	    pass
 		
 	def tick(self):
-		pass
+	    pass
