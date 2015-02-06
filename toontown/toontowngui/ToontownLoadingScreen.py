17b6dc0517488512ab52f213341b3fdc579bff15
Puggyblue999 <Puggyblue999@users.noreply.github.com>
2015-02-05 22:38:46 -0500

    The start of rewriting the loading screen. I need ideas for new loading screen!

--- toontown/toontowngui/ToontownLoadingScreen.py
+++ toontown/toontowngui/ToontownLoadingScreen.py
@@ -1,72 +1,29 @@
-from direct.gui.DirectGui import *
 from pandac.PandaModules import *
+from direct.gui.DirectGui import *
 from toontown.toonbase import ToontownGlobals
 from toontown.toonbase import TTLocalizer
 import random
 
-class ToontownLoadingScreen():
-    __module__ = __name__
 
-    def __init__(self):
-        self.__expectedCount = 0
-        self.__count = 0
-        self.gui = loader.loadModel('phase_3/models/gui/progress-background')
-        self.banner = loader.loadModel('phase_3/models/gui/toon_council').find('**/scroll')
-        self.banner.reparentTo(self.gui)
-        self.banner.setScale(0.4, 0.4, 0.4)
-        self.tip = DirectLabel(guiId='ToontownLoadingScreenTip', parent=self.banner, relief=None, text='', text_scale=TTLocalizer.TLStip, textMayChange=1, pos=(-1.2, 0.0, 0.1), text_fg=(0.4, 0.3, 0.2, 1), text_wordwrap=13, text_align=TextNode.ALeft)
-        self.title = DirectLabel(guiId='ToontownLoadingScreenTitle', parent=self.gui, relief=None, pos=(-1.06, 0, -0.77), text='', textMayChange=1, text_scale=0.08, text_fg=(0.00, 0.58039215686274509803921568627451, 0.9960784313725490196078431372549, 1), text_align=TextNode.ALeft, text_font=ToontownGlobals.getSignFont())
-        self.waitBar = DirectWaitBar(guiId='ToontownLoadingScreenWaitBar', parent=self.gui, frameSize=(-1.06,
-         1.06,
-         -0.03,
-         0.03), pos=(0, 0, -0.85), text='')
-        logoScale = 0.75  # Locked aspect ratio scale.
-        self.logo = OnscreenImage(
-            image='phase_3/maps/toontown-logo.png',
-            scale=(((16.0 / 9.0)*logoScale) / (4.0/3.0), 1, logoScale / (4.0/3.0)))
-        self.logo.reparentTo(hidden)
-        self.logo.setTransparency(TransparencyAttrib.MAlpha)
-        scale = self.logo.getScale()
-        #self.logo.setPos(scale[0], 0, -scale[2])
-        self.logo.setPos(0, 0, -scale[2])
-        return
-
-    def destroy(self):
-        self.tip.destroy()
-        self.title.destroy()
-        self.waitBar.destroy()
-        self.banner.removeNode()
-        self.gui.removeNode()
-
-    def getTip(self, tipCategory):
-        return TTLocalizer.TipTitle + '\n' + random.choice(TTLocalizer.TipDict.get(tipCategory))
-
-    def begin(self, range, label, gui, tipCategory):
-        self.waitBar['range'] = range
-        self.title['text'] = label
-        self.tip['text'] = self.getTip(tipCategory)
-        self.__count = 0
-        self.__expectedCount = range
-        if gui:
-            self.waitBar.reparentTo(self.gui)
-            self.title.reparentTo(self.gui)
-            self.gui.reparentTo(aspect2dp, NO_FADE_SORT_INDEX)
-        else:
-            self.waitBar.reparentTo(aspect2dp, NO_FADE_SORT_INDEX)
-            self.title.reparentTo(aspect2dp, NO_FADE_SORT_INDEX)
-            self.gui.reparentTo(hidden)
-        self.waitBar.update(self.__count)
-
-    def end(self):
-        self.waitBar.finish()
-        self.waitBar.reparentTo(self.gui)
-        self.title.reparentTo(self.gui)
-        self.gui.reparentTo(hidden)
-        return (self.__expectedCount, self.__count)
-
-    def abort(self):
-        self.gui.reparentTo(hidden)
-
-    def tick(self):
-        self.__count = self.__count + 1
-        self.waitBar.update(self.__count)
+class ToontownLoadingScreen:
+	
+	def __init__(self):
+		pass
+		
+	def destroy(self):
+		pass
+		
+	def getTip(self, todo):
+		pass
+		
+	def begin(self, todo, todo1, todo2, todo3):
+		pass
+		
+	def end(self):
+		pass
+		
+	def abort(self):
+		pass
+		
+	def tick(self):
+		pass
