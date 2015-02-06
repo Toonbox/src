d794923edb453c33282d4415843b0ade6bdb8998
Puggyblue999 <Puggyblue999@users.noreply.github.com>
2015-02-06 16:19:35 -0500

    Start of working on loading screen & added stuff back to LoadingScreen_TTI

--- toontown/toontowngui/ToontownLoadingScreen.py
+++ toontown/toontowngui/ToontownLoadingScreen.py
@@ -7,8 +7,45 @@ import random
 
 class ToontownLoadingScreen:
 	
+	defaultTex = 'phase_3.5/maps/loading/default.jpg'
+	zone2video = {
+	    ToontownGlobals.ToontownCentral : '',
+	    ToontownGlobals.SillyStreet : '',
+	    ToontownGlobals.LoopyLane : '',
+	    ToontownGlobals.PunchlinePlace : '',
+	    ToontownGlobals.DonaldsDock : '',
+	    ToontownGlobals.BarnacleBoulevard : '',
+	    ToontownGlobals.SeaweedStreet : '',
+	    ToontownGlobals.LighthouseLane : '',
+	    ToontownGlobals.TheBrrrgh : '',
+	    ToontownGlobals.WalrusWay : '',
+	    ToontownGlobals.SleetStreet : '',
+	    ToontownGlobals.PolarPlace : '',
+	    ToontownGlobals.MinniesMelodyland : '',
+	    ToontownGlobals.AltoAvenue : '',
+	    ToontownGlobals.BaritoneBoulevard : '',
+	    ToontownGlobals.TenorTerrace : '',
+	    ToontownGlobals.DaisyGardens : '',
+	    ToontownGlobals.ElmStreet : '',
+	    ToontownGlobals.MapleStreet : '',
+	    ToontownGlobals.OakStreet : '',
+	    ToontownGlobals.DonaldsDreamland : '',
+	    ToontownGlobals.LullabyLane : '',
+	    ToontownGlobals.OutdoorZone : '',
+	    ToontownGlobals.GolfZone : '',
+	    ToontownGlobals.GoofySpeedway : '',
+	    ToontownGlobals.BossbotHQ : '',
+	    ToontownGlobals.SellbotHQ : '',
+	    ToontownGlobals.CashbotHQ : '',
+	    ToontownGlobals.LawbotHQ : '',
+	    ToontownGlobals.Tutorial : '',
+	    ToontownGlobals.MyEstate : ''
+	}
+	
 	def __init__(self):
-		pass
+		self.__expectedCount = 0
+		self.__count = 0
+		self.gui = loader.loadModel('phase_3/models/gui/progress-background.bam')
 		
 	def destroy(self):
 		pass
